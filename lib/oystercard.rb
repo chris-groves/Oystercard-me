e0e5b9544fa6d1e6a7b38063ad5d6bf6355ebc50
Chris Groves <chris_groves87@hotmail.co.uk>
2019-09-22 08:59:59 +0100

    feat: card prevents balance exceeding £90

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,4 +1,7 @@
 class Oystercard
+
+  BALANCE_LIMIT = 90
+
   attr_reader :balance
 
   def initialize
@@ -6,6 +9,7 @@ class Oystercard
   end
 
   def top_up(amount)
+    raise "Error: balance will exceed £#{BALANCE_LIMIT}" if @balance + amount > BALANCE_LIMIT
     @balance += amount
   end
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -9,4 +9,10 @@ describe Oystercard do
     expect { subject.top_up(5) }.to change { subject.balance }.by(5)
   end
 
+  it 'has a maximum balance of 90' do
+    subject.top_up(Oystercard::BALANCE_LIMIT)
+    message = "Error: balance will exceed £#{Oystercard::BALANCE_LIMIT}"
+    expect { subject.top_up(5) }.to raise_error message
+  end
+
 end
